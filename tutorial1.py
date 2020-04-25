43ce7ab68f90888fc6a6541037f8c924fd285694
oliver <oliver.lell@gmx.at>
2020-04-25 14:47:33 +0200

    →server push

--- tutorial1.py
+++ tutorial1.py
@@ -21,7 +21,7 @@ def collect_data():
                 # print("ga")
                 data = conn.recv(1024)
                 rd = []
-                a_dict = json.loads()
+                a_dict = json.loads(data)
                 print(dict(a_dict))
                 for v in data:
                     rd.append(int(v))
