c2499fc30689b893ace37d647fdf08265f788965
oliver <oliver.lell@gmx.at>
2020-04-28 11:13:33 +0200

    →server push

--- TTT.py
+++ TTT.py
@@ -6,6 +6,7 @@ class TTT:
     def doSomething(self,message):
         if  message!= None:
             message = json.loads(message.decode('utf-8'))
+            print("message in != null "+ message)
             if(message.get('Atktion')==None):
                 print("in none weil aktion null")
                 return None
