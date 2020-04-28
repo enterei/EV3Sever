af04c88071df21584c31129d36fc1db112fb62b0
oliver <oliver.lell@gmx.at>
2020-04-28 09:51:23 +0200

    →server push

--- TTT.py
+++ TTT.py
@@ -4,9 +4,12 @@ class TTT:
     def __init__(self, **kwargs):
         print("init")
     def doSomething(self,message):
-        message = json.loads(message.decode('utf-8'))
-        if(message.get('Atktion')==None):
-            print("none")
+        if not message:
+            message = json.loads(message.decode('utf-8'))
+            if(message.get('Atktion')==None):
+                print("none")
+                return None
+            if(message.get('Aktion')=="Ecke"):
+                return json.dumps({'Aktion':"messen"}).encode('uft-8')
+        else:
             return None
-        if(message.get('Aktion')=="Ecke"):
-            return json.dumps({'Aktion':"messen"}).encode('uft-8')
