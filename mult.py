325670fb9cfa0fb8235466c5089b2b37323aea44
oliver <oliver.lell@gmx.at>
2020-05-19 11:26:39 +0200

    →server push

--- mult.py
+++ mult.py
@@ -9,23 +9,26 @@ from TTT import TTT
 parser = argparse.ArgumentParser(description="rosolve robo modes")
 parser.add_argument('--rmode',type=str,help='rmode!',required=False)
 parser.add_argument('--cmode',type=int,default=1,help='cmode!',required=False)
-parser.add_argument('--speed',type=int,default=5,help='speed!',required=False)
+parser.add_argument('--speed',type=int,default=10,help='speed!',required=False)
 parser.add_argument('--time',type=int,default=10,help='time!',required=False)
 parser.add_argument('--ms',type=int,default=4500,help='ms!',required=False)
-parser.add_argument('--edgemax',type=int,default=2,help='edgemax!',required=False)
-parser.add_argument('--edgev',type=int,default=21,help='tagarget_light_intensity!',required=False)
+parser.add_argument('--edgemax',type=int,default=1,help='edgemax!',required=False)
+parser.add_argument('--edgev',type=int,default=53,help='tagarget_light_intensity!',required=False)
 parser.add_argument('--bscan',type=bool,default=False,help='bscan!',required=False)
 #follow
-parser.add_argument('--kp',type=float,default=3,help='kp!',required=False)
-parser.add_argument('--ki',type=float,default=0.05,help='ki!',required=False)
-parser.add_argument('--kd',type=float,default=0.2,help='kd!',required=False)
-parser.add_argument('--tlr',type=float,default=36,help='tlr!',required=False)
+parser.add_argument('--kp',type=float,default=2,help='kp!',required=False)
+parser.add_argument('--ki',type=float,default=0.0,help='ki!',required=False)
+parser.add_argument('--kd',type=float,default=0.3,help='kd!',required=False)
+parser.add_argument('--tlr',type=float,default=62,help='tlr!',required=False)
+parser.add_argument('--wsp',type=int,default=76,help='wsp!',required=False)
+
 #### turn
 parser.add_argument('--lspeed',type=int,default=20,help='rsped!',required=False)
 parser.add_argument('--rspeed',type=int,default=-5,help='lspeed!',required=False)
 parser.add_argument('--turn',type=bool,default=False,help='turn!',required=False)
-parser.add_argument('--degrees',type=int,default=200,help='deg!',required=False)
+parser.add_argument('--degrees',type=int,default=250,help='deg!',required=False)
 parser.add_argument('--rot',type=float,default=1,help='rot!',required=False)
+parser.add_argument('--edgetest',type=bool,default=False,help='edgetest!',required=False)
 
 
 parser.add_argument('--back',type=bool,default=False,help='back!',required=False)
@@ -109,7 +112,9 @@ defaultM={'speed':pargs.speed,'time':pargs.time,'ms':pargs.ms,
                           'edgemax':pargs.edgemax,'bscan':pargs.bscan,'edgev':pargs.edgev,
                           'ki':pargs.ki,'kd':pargs.kd,'kp':pargs.kp,
                           'turn':pargs.turn,'lspeed':pargs.lspeed,'rspeed':pargs.rspeed,'degrees':pargs.degrees,
-                          'rot':pargs.rot,'target_light_intensity':pargs.tlr,'way':pargs.way,'back':pargs.back
+                          'rot':pargs.rot,'target_light_intensity':pargs.tlr,'way':pargs.way,'back':pargs.back,
+                            'wsp':pargs.wsp,'edgetest':pargs.edgetest
+
                           }
 # ...
 message_handelr=SystemHandler(defaultM)
