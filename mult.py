c632516a51373928143acbc0824d5be864a48aaf
oliver <oliver.lell@gmx.at>
2020-09-18 13:45:17 +0200

    →server push

--- mult.py
+++ mult.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser(description="rosolve robo modes")
 
 
 parser.add_argument('--ms',type=int,default=4500,help='ms!',required=False)
-parser.add_argument('--cornerC ',type=int,default=1,help='cornerC !',required=False) #how often count dark value to stop
+parser.add_argument('--cornerC',type=int,default=1,help='cornerC !',required=False) #how often count dark value to stop
 parser.add_argument('--cornervalue',type=int,default=54,help='cornervalue!',required=False) # how dark to stop at corner
 #line following arguments
 parser.add_argument('--kp',type=float,default=2,help='kp!',required=False)
@@ -86,7 +86,7 @@ sel = selectors.DefaultSelector()
 pargs = parser.parse_args()
 
 defaultM={'speed':pargs.speed,'time':pargs.time,'ms':pargs.ms,
-                          'edgemax':pargs.corner ,'bscan':pargs.bscan,'edgev':pargs.cornervalue,
+                          'edgemax':pargs.cornerC ,'bscan':pargs.bscan,'edgev':pargs.cornervalue,
                           'ki':pargs.ki,'kd':pargs.kd,'kp':pargs.kp,
                           'turn':pargs.turn,'lspeed':pargs.turnls,'rspeed':pargs.turnrs,'degrees':pargs.initial_rot,
                           'rot':pargs.adjust_rot,'target_light_intensity':pargs.tlr,'way':pargs.way,'back':pargs.back,
