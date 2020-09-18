2886d19404dca7657e2e137e780cd2565b59bd08
oliver <oliver.lell@gmx.at>
2020-09-18 13:41:47 +0200

    →server push

--- mult.py
+++ mult.py
@@ -7,38 +7,40 @@ import types
 from SystemHandler import SystemHandler
 from TTT import TTT
 parser = argparse.ArgumentParser(description="rosolve robo modes")
-parser.add_argument('--rmode',type=str,help='rmode!',required=False)
-parser.add_argument('--cmode',type=int,default=1,help='cmode!',required=False)
-parser.add_argument('--speed',type=int,default=7,help='speed!',required=False)
-#parser.add_argument('--speed',type=int,default=7,help='speed!',required=False)
-parser.add_argument('--time',type=int,default=10,help='time!',required=False)
+
+
+
 parser.add_argument('--ms',type=int,default=4500,help='ms!',required=False)
-parser.add_argument('--edgemax',type=int,default=1,help='edgemax!',required=False)
-parser.add_argument('--edgev',type=int,default=54,help='tagarget_light_intensity!',required=False)
-parser.add_argument('--bscan',type=bool,default=False,help='bscan!',required=False)
-#follow
+parser.add_argument('--cornerC ',type=int,default=1,help='cornerC !',required=False) #how often count dark value to stop
+parser.add_argument('--cornervalue',type=int,default=54,help='cornervalue!',required=False) # how dark to stop at corner
+#line following arguments
 parser.add_argument('--kp',type=float,default=2,help='kp!',required=False)
-parser.add_argument('--ki',type=float,default=0.0,help='ki!',required=False)
+parser.add_argument('--ki',type=float,default=0.001,help='ki!',required=False)
 parser.add_argument('--kd',type=float,default=0.3,help='kd!',required=False)
 parser.add_argument('--tlr',type=float,default=62,help='tlr!',required=False)
 parser.add_argument('--wsp',type=int,default=78,help='wsp!',required=False)
+parser.add_argument('--speed',type=int,default=7,help='speed!',required=False)
 
-#### turnsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-parser.add_argument('--lspeed',type=int,default=0,help='rsped!',required=False)
-parser.add_argument('--rspeed',type=int,default=5,help='lspeed!',required=False)
+#turning arguments
+parser.add_argument('--turnls',type=int,default=0,help='turnls!',required=False) #left speed
+parser.add_argument('--turnrs',type=int,default=5,help='turnrs!',required=False)#right speed
+
+parser.add_argument('--initial_rot',type=int,default=120,help='initial_rot!',required=False) #turn for this degrees
+parser.add_argument('--adjust_rot',type=float,default=0,help='rot!',required=False)
+parser.add_argument('--adjust_s',type=int,default=1,help='adjust_s!',required=False)
+parser.add_argument('--initial_ls',type=float,default=7,help='initial_ls!',required=False)
+parser.add_argument('--initial_rs',type=float,default=-3,help='initial_ls!',required=False)
+#testing arguments
+parser.add_argument('--edgetest',type=bool,default=False,help='edgetest!',required=False) #test edges
+parser.add_argument('--back',type=bool,default=False,help='back!',required=False) # drive backwards
+parser.add_argument('--way',type=str,help='way!',required=False) # way as string("s","l","r")
+parser.add_argument('--rmode',type=str,help='rmode!',required=False)  #running mode
+parser.add_argument('--cmode',type=int,default=1,help='cmode!',required=False)# color sensor mode
+parser.add_argument('--time',type=int,default=10,help='time!',required=False) #follow line for ms seconds
 parser.add_argument('--turn',type=bool,default=False,help='turn!',required=False)
-parser.add_argument('--degrees',type=int,default=120,help='deg!',required=False)
-parser.add_argument('--rot',type=float,default=0,help='rot!',required=False)
-parser.add_argument('--edgetest',type=bool,default=False,help='edgetest!',required=False)
-
-parser.add_argument('--uspeed',type=int,default=1,help='deg!',required=False)
-parser.add_argument('--ulspeed',type=float,default=7,help='rot!',required=False)
-parser.add_argument('--urspeed',type=float,default=-3,help='rot!',required=False)
-
-
-parser.add_argument('--back',type=bool,default=False,help='back!',required=False)
-
-parser.add_argument('--way',type=str,help='way!',required=False)
+parser.add_argument('--bscan',type=bool,default=False,help='bscan!',required=False) #testing
+parser.add_argument('--port',type=str,default=65432,help='turn!',required=False)
+parser.add_argument('--host',type=str,default='192.168.0.179',help='bscan!',required=False) #testing
 
 
 messageOut=None
@@ -84,18 +86,18 @@ sel = selectors.DefaultSelector()
 pargs = parser.parse_args()
 
 defaultM={'speed':pargs.speed,'time':pargs.time,'ms':pargs.ms,
-                          'edgemax':pargs.edgemax,'bscan':pargs.bscan,'edgev':pargs.edgev,
+                          'edgemax':pargs.cornerC ,'bscan':pargs.bscan,'edgev':pargs.cornervalue,
                           'ki':pargs.ki,'kd':pargs.kd,'kp':pargs.kp,
-                          'turn':pargs.turn,'lspeed':pargs.lspeed,'rspeed':pargs.rspeed,'degrees':pargs.degrees,
-                          'rot':pargs.rot,'target_light_intensity':pargs.tlr,'way':pargs.way,'back':pargs.back,
-                            'wsp':pargs.wsp,'edgetest':pargs.edgetest,'ulspeed':pargs.ulspeed,'urspeed':pargs.urspeed,'uspeed':pargs.uspeed
+                          'turn':pargs.turn,'lspeed':pargs.turnls,'rspeed':pargs.turnrs,'degrees':pargs.initial_rot,
+                          'rot':pargs.adjust_rot,'target_light_intensity':pargs.tlr,'way':pargs.way,'back':pargs.back,
+                            'wsp':pargs.wsp,'edgetest':pargs.edgetest,'ulspeed':pargs.initial_ls,'urspeed':pargs.initial_rs,'uspeed':pargs.adjust_s
 
                           }
 # ...
 
 MessageHandler=SystemHandler(defaultM)
-host = '192.168.0.179'  # The server's hostname or IP address
-port = 65432
+host = pargs.host  # The server's hostname or IP address
+port = pargs.port
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
